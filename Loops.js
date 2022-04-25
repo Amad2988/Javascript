0ec528d4eeca77dbc647bd572a4aefd79e3205d9
Amad2988 <102828487+Amad2988@users.noreply.github.com>
2022-04-25 11:02:48 -0400

    Add files via upload

---
+++ Loops.js
@@ -0,0 +1,40 @@
+//Exercise 1
+// for(i = 0; i <=9; i++){
+//     console.log(i + 1);
+//     i++
+// }
+// for(i = 2; i <=10; i++){
+//     console.log(i);
+//     i++
+// }
+
+//Exercise 2
+
+// for(i = 10; i >= 1; i--) {
+//     console.log(i);
+// }
+
+//Excerise 3
+
+// for(i = 6; i <= 60; i++) {
+//     if (i % 3 === 0){
+//         console.log(i);
+//     }
+    
+// }
+
+//Excerise 4
+
+// for(i = 0; i <= 10; i++) 
+ //{
+   // if (i % 2 === 0)
+    //console.log(i);        
+//}
+
+
+//Exercise 5
+
+//for( i='#'.length; i<=7; i++){
+//    console.log('#'.repeat(i))
+//}
+  
