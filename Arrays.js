42d5221195274aa8616d72e7977d59a6a83438ca
Amad2988 <102828487+Amad2988@users.noreply.github.com>
2022-04-25 14:49:03 -0400

    Add files via upload

---
+++ Arrays.js
@@ -0,0 +1,22 @@
+//const myArray = [Math.floor(Math.random() * 10),3,4,5]
+//console.log(Array.isArray(myArray))
+
+//myArray[2] = 78;
+
+//console.log(myArray)
+
+const netflixShows = ["Orange is the New Black", "Black Mirror", "Chewing Gum"];
+
+// ADD CODE HERE
+netflixShows.push('Squid Game')
+
+// Write a console.log statement below to check your work!
+console.log(netflixShows)
+
+const horror = ['Freddy', 'Jason', 'Michael', 'Ghostface', 'Chucky'];
+
+// ADD CODE HERE
+const fourthItem = horror[3]
+
+// Write a console.log statement below to check your work!
+console.log(fourthItem)
